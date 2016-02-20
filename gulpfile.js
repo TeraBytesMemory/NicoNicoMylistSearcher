a420d9af3470efa4954933f04de030b0e117d1c1
rbskeleton <red.big.skeleton.blaze@gmail.com>
2016-02-20 10:52:35 +0900

    ready to deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,11 @@
 var gulp = require('gulp');
-var jasmine = require('gulp-jasmine');
 var $ = require("gulp-load-plugins")();
 var webpackConfig = require('./webpack.config.js');
-var Karma = require('karma').Server;
 
 
 gulp.task('default', function () {
     gulp.watch(['./src/*.js', './src/*.jsx',
                 '!/src/flycheck*.js', '!/src/flycheck*.jsx'], ['build']);
-    gulp.watch(['./spec/support/*spec.js', './spec/support/*spec.jsx',
-                '!/spec/support/flycheck_*.js', '!/spec/support/flycheck_*.jsx'],
-               ['test']);
 });
 
 gulp.task('build', function () {
@@ -18,14 +13,6 @@ gulp.task('build', function () {
                      './src/*/*.js', './src/*/*.jsx'])
         .pipe($.plumber())
         .pipe($.webpack(webpackConfig))
-        .pipe(gulp.dest('build/'));
-});
-
-gulp.task('test', function() {
-    gulp.src(['./spec/support/*spec.js', './spec/support/*spec.jsx'])
-        .pipe($.plumber())
-        .pipe(new Karma({
-            configFile: __dirname + '/karma.conf.js',
-            action: 'watch'
-        }).start());
+        .pipe($.uglify())
+        .pipe(gulp.dest(''));
 });
